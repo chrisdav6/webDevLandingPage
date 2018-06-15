81611ec6a7238e4f435216cb0f6bcbf4a3678db5
chrisdav6 <chris.davis5440@gmail.com>
2018-06-15 15:52:00 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(function() {
+
+  $("body").animate({
+    opacity: 1
+  }, 3000);
+
+  $(".btn").hover(
+    function() {
+    $(".image-bg").css("background-size", "120% 120%");
+    $(".overlay").animate({
+      backgroundColor: "rgba(0, 0, 0, .9)"
+    },1000);
+    $(".fas").css({"left":"-5px", "width":"30px", "opacity": "1"});
+  }, function() {
+    $(".image-bg").css("background-size", "100% 100%");
+    $(".overlay").animate({
+      backgroundColor: "rgba(0, 0, 0, .6)"
+    }, 1000);
+    $(".fas").css({ "left": "-40px", "width": "0px", "opacity": "0"});
+  });
+
+});
