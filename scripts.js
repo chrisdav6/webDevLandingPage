2f127182c9a32109a12b0afa96e6f9e6f5d80186
chrisdav6 <chris.davis5440@gmail.com>
2018-06-18 12:11:53 -0400

    Added stop method to jquery overlay animation to clear animation on multiple hovers

--- scripts.js
+++ scripts.js
@@ -7,13 +7,13 @@ $(function() {
   $(".btn").hover(
     function() {
     $(".image-bg").css("background-size", "120% 120%");
-    $(".overlay").animate({
+    $(".overlay").stop().animate({
       backgroundColor: "rgba(0, 0, 0, .9)"
     },1000);
     $(".fas").css({"left":"-5px", "width":"30px", "opacity": "1"});
   }, function() {
     $(".image-bg").css("background-size", "100% 100%");
-    $(".overlay").animate({
+    $(".overlay").stop().animate({
       backgroundColor: "rgba(0, 0, 0, .6)"
     }, 1000);
     $(".fas").css({ "left": "-40px", "width": "0px", "opacity": "0"});
